74f157994ebb3a88f5622964b52315d2b71bf2d9
Samm <60794497+Sammyueru@users.noreply.github.com>
2023-01-15 09:57:12 -0700

    Update saveimage.js

--- saveimage.js
+++ saveimage.js
@@ -18,11 +18,11 @@
   
   function hexToRgb(hex) {
     var result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hex);
-    return result ? {
+    return result {
       r: parseInt(result[1], 16),
       g: parseInt(result[2], 16),
       b: parseInt(result[3], 16)
-    } : null;
+    };
   }
 
   const download = (file, img_type='png', width=255, pixels) => {
