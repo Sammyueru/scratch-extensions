2ac32f4046ae1a73a43db1157be7995dc49ffe80
Samm <60794497+Sammyueru@users.noreply.github.com>
2023-01-15 10:03:46 -0700

    Update saveimage.js

--- saveimage.js
+++ saveimage.js
@@ -18,10 +18,11 @@
   
   function hexToRgb(hex) {
     var result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hex);
-    return result {
-      r: parseInt(result[1], 16),
-      g: parseInt(result[2], 16),
-      b: parseInt(result[3], 16)
+    const r = parseInt(result[1], 16);
+    const g = parseInt(result[2], 16);
+    const b = parseInt(result[3], 16);
+    return {
+      r,g,b
     };
   }
 
